0efa941ee2962c85ab4687f1be6daf286a3047e0
kibet kilel <amos.rotich@strathmore.edu>
2022-06-21 08:06:22 +0300

    create models

--- I4G017970JAF/blog/models.py
+++ I4G017970JAF/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    Created_date = models.DateTimeField
+    Published_date = models.DateTimeField
+
+    def __str__(self):
+        return self.title
